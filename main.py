53490a82f695d33d029776d17b0a3016666192af
Emrah ERTAN <75017522+emrah-ertan@users.noreply.github.com>
2023-11-18 19:27:50 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,12 @@
 import Convert
 import Update
 
-def son_isleme(image_path):
-    Convert.to_bitmap(image_path)
-    Convert.to_svg_potrace_exe("converted_image.bmp")
-    Update.renklendir("converted_image.svg")
 
-son_isleme("GeneratedImages/kergit.jpg")
 """""
 #MENU
 user_input = input("Enter input:")
 
-image = GenerateImage.generate_image(user_input,GenerateImage.image_gen_model)
+image = Generate.generateImage(user_input,GenerateImage.image_gen_model)
 image = cv2.cvtColor(np.array(image),cv2.COLOR_RGB2BGR)
 
 
@@ -19,8 +14,8 @@ cv2.imshow("Generated Image",image)
 user_input = user_input.replace(" ","")
 cv2.imwrite("GeneratedImages/"+user_input+".jpg",image)
 
-#ConvertImage.to_bitmap("GeneratedImages/"+user_input+".jpg")
-#ConvertImage.to_svg_potrace_exe("converted_image.bmp")
+#Convert.to_bitmap("GeneratedImages/"+user_input+".jpg")
+#Convert.to_svg_potrace_exe("converted_image.bmp")
 
 
 cv2.waitKey(0)
