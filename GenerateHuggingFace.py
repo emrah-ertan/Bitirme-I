5c1194aa390ef12a4a855fe1016e5c9761e8bd69
Monster <emrahertan@yaani.com>
2023-12-18 14:09:11 +0300

    last

---
+++ GenerateHuggingFace.py
@@ -0,0 +1,25 @@
+import Generate
+import Convert
+import Update
+import cv2
+import numpy as np
+
+
+user_input = "Tulip Pattern"
+
+
+
+image = Generate.generateImage(user_input,Generate.image_gen_model)
+image = cv2.cvtColor(np.array(image),cv2.COLOR_RGB2BGR)
+
+
+cv2.imshow("Generated Image",image)
+user_input = user_input.replace(" ","")
+cv2.imwrite("GeneratedImages/"+user_input+".jpg",image)
+
+Convert.to_bitmap("GeneratedImages/"+user_input+".jpg")
+Convert.to_svg_potrace_exe("image.bmp")
+Update.renklendir(image.svg)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
