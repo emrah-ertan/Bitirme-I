6084144035f3b236285854a6bb64f395001fd0a3
Monster <emrahertan@yaani.com>
2024-01-15 14:02:42 +0300

    last

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import Scale
 import os
 import time
 
-userPrompt = "Portal of glory"
+userPrompt = "Blank Canvas"
 adimSayisi = 10
 
 blackLevel = 0.5
@@ -16,82 +16,123 @@ mainSeed = np.random.choice([1, 2, 3, 4, 5, 6, 7, 8, 9])
 
 
 def run():
-    startTime1 = time.time()
-    import GenerateCivitai
+    #Mevcut Dosyaların Silinmesi
+
+    #png dosyalarını sil
+    """""if os.path.exists("GeneratedImages/imageStablev15.png"):
+        os.remove("GeneratedImages/imageStablev15.png")
+    if os.path.exists("GeneratedImages/imageStablev15ClipSkip.png"):
+        os.remove("GeneratedImages/imageStablev15ClipSkip.png")
+    if os.path.exists("GeneratedImages/imageStablev14.png"):
+        os.remove("GeneratedImages/imageStablev14.png")
+    if os.path.exists("GeneratedImages/imageStability.png"):
+        os.remove("GeneratedImages/imageStability.png")
+    if os.path.exists("GeneratedImages/imageKANDINSKY.png"):
+        os.remove("GeneratedImages/imageKANDINSKY.png")
+    if os.path.exists("GeneratedImages/imageMiniDalle2.png"):
+        os.remove("GeneratedImages/imageMiniDalle2.png")
+
+    #svg dosyalarını sil
+    if os.path.exists("GeneratedImages/vector_graphicStablev15.svg"):
+        os.remove("GeneratedImages/vector_graphicStablev15.svg")
+    if os.path.exists("GeneratedImages/vector_graphicStablev15ClipSkip.svg"):
+        os.remove("GeneratedImages/vector_graphicStablev15ClipSkip.svg")
+    if os.path.exists("GeneratedImages/vector_graphicStablev14.svg"):
+        os.remove("GeneratedImages/vector_graphicStablev14.svg")
+    if os.path.exists("GeneratedImages/vector_graphicStability.svg"):
+        os.remove("GeneratedImages/vector_graphicStability.svg")
+    if os.path.exists("GeneratedImages/vector_graphicKANDINSKY.svg"):
+        os.remove("GeneratedImages/vector_graphicKANDINSKY.svg")
+    if os.path.exists("GeneratedImages/vector_graphicMiniDalle2.svg"):
+        os.remove("GeneratedImages/vector_graphicMiniDalle2.svg")
+
+    #scaled svg dosyalarını sil
+    if os.path.exists("GeneratedImages/vector_graphic_scaledStablev15.svg"):
+        os.remove("GeneratedImages/vector_graphic_scaledStablev15.svg")
+    if os.path.exists("GeneratedImages/vector_graphic_scaledStablev15ClipSkip.svg"):
+        os.remove("GeneratedImages/vector_graphic_scaledStablev15ClipSkip.svg")
+    if os.path.exists("GeneratedImages/vector_graphic_scaledStablev14.svg"):
+        os.remove("GeneratedImages/vector_graphic_scaledStablev14.svg")
+    if os.path.exists("GeneratedImages/vector_graphic_scaledStability.svg"):
+        os.remove("GeneratedImages/vector_graphic_scaledStability.svg")
+    if os.path.exists("GeneratedImages/vector_graphic_scaledKANDINSKY.svg"):
+        os.remove("GeneratedImages/vector_graphic_scaledKANDINSKY.svg")
+    if os.path.exists("GeneratedImages/vector_graphic_scaledMiniDalle2.svg"):
+        os.remove("GeneratedImages/vector_graphic_scaledMiniDalle2.svg")"""""
+
+
+    #Mevcut dosyalar silindikten sonra yeni dosyaların oluşturulması
+    """""import GenerateCivitai
     while True:
         if os.path.exists("GeneratedImages/imageStablev15.png"):
             break
         else:
             continue
-    endTime1 = time.time()
-    elapsedTime1 = endTime1 - startTime1
-
-    startTime2 = time.time()
     import GenerateCivitaiClipSkip
     while True:
         if os.path.exists("GeneratedImages/imageStablev15ClipSkip.png"):
             break
         else:
             continue
-    endTime2 = time.time()
-    elapsedTime2 = endTime2 - startTime2
-    startTime3 = time.time()
     import GenerateHuggingF
     while True:
         if os.path.exists("GeneratedImages/imageStablev14.png"):
             break
         else:
             continue
-    endTime3 = time.time()
-    elapsedTime3= endTime3 - startTime3
-    """""startTime4 = time.time()
-    import GenerateStabilityai"
+    import GenerateStabilityai
     while True:
         if os.path.exists("GeneratedImages/imageStability.png"):
             break
         else:
             continue
-    endTime4= time.time()
-    elapsedTime= endTime4-startTime4"""""
-    startTime5 = time.time()
     import GenerateKANDINSKY
     while True:
         if os.path.exists("GeneratedImages/imageKANDINSKY.png"):
             break
         else:
             continue
-    endTime5 =time.time()
-    elapsedTime5 = endTime5 - startTime5
-    startTime6 = time.time()
     import GenerateMiniDalle2
     while True:
         if os.path.exists("GeneratedImages/imageMiniDalle2.png"):
             break
         else:
-            continue
-    endTime6 =time.time()
-    elapsedTime6 = endTime6 - startTime6
-
-
-    print(f"Elapsed Time1: {elapsedTime1}")
-    print(f"Elapsed Time2: {elapsedTime2}")
-    print(f"Elapsed Time3: {elapsedTime3}")
-    #print(f"Elapsed Time4: {elapsedTime4}")
-    print(f"Elapsed Time5: {elapsedTime5}")
-    print(f"Elapsed Time6: {elapsedTime6}")
+            continue"""""
+    print(f"Main.py run() fonksiyonu çalıştı. Gelen prompt: {userPrompt}")
+    print(f"Main.py run() fonksiyonu çalıştı. Gelen adım sayısı: {adimSayisi}")
+    print(f"Dönüş değeri 1 olarak belirlendi ve döndürülüyor")
+    return 1
 
 
 def indir():
-    Convert.to_svg_openPotrace("GeneratedImages/imageStablev15.png", blackLevel)
-    Convert.to_svg_openPotrace("GeneratedImages/imageStablev15ClipSkip.png",blackLevel)
-    Convert.to_svg_openPotrace("GeneratedImages/imageStablev14.png", blackLevel)
-    # Convert.to_svg_openPotrace("GeneratedImages/imageStability.png", blackLevel)
-    Convert.to_svg_openPotrace("GeneratedImages/imageKANDINSKY.png",blackLevel)
-    Convert.to_svg_openPotrace("GeneratedImages/imageMiniDalle2.png",blackLevel)
+    """""if os.path.exists("GeneratedImages/imageStablev15.png"):
+        Convert.to_svg_openPotrace("GeneratedImages/imageStablev15.png", blackLevel)
+    if os.path.exists("GeneratedImages/imageStablev15ClipSkip.png"):
+        Convert.to_svg_openPotrace("GeneratedImages/imageStablev15ClipSkip.png",blackLevel)
+    if os.path.exists("GeneratedImages/imageStablev14.png"):
+        Convert.to_svg_openPotrace("GeneratedImages/imageStablev14.png", blackLevel)
+    #if os.path.exists("GeneratedImages/imageStability.png"):
+        # Convert.to_svg_openPotrace("GeneratedImages/imageStability.png", blackLevel)
+    if os.path.exists("GeneratedImages/imageKANDINSKY.png"):
+        Convert.to_svg_openPotrace("GeneratedImages/imageKANDINSKY.png",blackLevel)
+    if os.path.exists("GeneratedImages/imageMiniDalle2.png"):
+        Convert.to_svg_openPotrace("GeneratedImages/imageMiniDalle2.png",blackLevel)
 
-    Scale.svg("GeneratedImages/vector_graphicStablev15.svg")
-    Scale.svg("GeneratedImages/imageStablev15ClipSkip.svg")
-    Scale.svg("GeneratedImages/vector_graphicStablev14.svg")
-    #Scale.svg("GeneratedImages/imageStability.svg")
-    Scale.svg("GeneratedImages/imageKANDINSKY.svg")
-    Scale.svg("GeneratedImages/imageMiniDalle2.svg")
+
+    if os.path.exists("GeneratedImages/vector_graphicStablev15.svg"):
+        Scale.svg("GeneratedImages/vector_graphicStablev15.svg")
+    if os.path.exists("GeneratedImages/vector_graphicStablev15ClipSkip.svg"):
+        Scale.svg("GeneratedImages/vector_graphicStablev15ClipSkip.svg")
+    if os.path.exists("GeneratedImages/vector_graphicStablev14.svg"):
+        Scale.svg("GeneratedImages/vector_graphicStablev14.svg")
+    #if os.path.exists("GeneratedImages/vector_graphicStability.svg"):
+        #Scale.svg("GeneratedImages/vector_graphicStability.svg")
+    if os.path.exists("GeneratedImages/vector_graphicKANDINSKY.svg"):
+        Scale.svg("GeneratedImages/vector_graphicKANDINSKY.svg")
+    if os.path.exists("GeneratedImages/vector_graphicMiniDalle2.svg"):
+        Scale.svg("GeneratedImages/vector_graphicMiniDalle2.svg")"""""
+
+    print(f"Main.py indir() fonksiyonu çalıştı. Gelen blackLevel: {blackLevel}")
+    print(f"Main.py indir() fonksiyonu çalıştı. Gelen ölçek: {olcek}")
+
+    return 1
